f12d09cf7c7d325e2cb37815bd57cdbd57394a00
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-09-22 12:23:30 +0300

    Overwritting M.ind logic DONE, though it has no ability to work

--- labs/lab1/source/file_functs.c
+++ labs/lab1/source/file_functs.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 // --------------------------------------- ADD MASTER ---------------------------------------
@@ -33,12 +34,86 @@ void add_master_idx(struct Metro metro) {
         return;
     }
 
-    fwrite(&metro.id, sizeof(metro.id), 1, fp);
-
     fseek(fp, 0, SEEK_END);
-    int data_num = ftell(fp) / (sizeof(metro.id) + sizeof(int));
+    int ind_row_size = sizeof(metro.id) + sizeof(int);
+    int data_loc = ftell(fp) / ind_row_size;
+    fseek(fp, 0, SEEK_SET);
 
-    fwrite(&data_num, sizeof(data_num), 1, fp);
+    int prev_id = -1, next_id = -1;
+    bool insertInside = false;
+
+    switch (data_loc)
+    {
+    case 0:
+        fseek(fp, 0, SEEK_END);
+        break;
+
+    case 1:
+        fread(&prev_id, sizeof(prev_id), 1, fp);
+        if (metro.id < prev_id) {
+            fseek(fp, 0, SEEK_SET);
+            insertInside = true;
+        }
+        else
+            fseek(fp, 0, SEEK_END);
+        break;
+
+    default:
+        fread(&prev_id, sizeof(prev_id), 1, fp);
+        fseek(fp, sizeof(int), SEEK_CUR);
+        fread(&next_id, sizeof(next_id), 1, fp);
+
+        if (metro.id < prev_id) {
+            fseek(fp, 0, SEEK_SET);
+            insertInside = true;
+            break;
+        }
+
+        do {
+            if (prev_id < metro.id && metro.id < next_id) {
+                fseek(fp, -1 * (int)sizeof(next_id), SEEK_CUR);
+                insertInside = true;
+                break;
+            }
+
+            prev_id = next_id;
+            fseek(fp, sizeof(int), SEEK_CUR);
+        } while (fread(&next_id, sizeof(next_id), 1, fp) != NULL);
+
+        fseek(fp, 0, SEEK_END);
+        break;
+    }
+
+    if (insertInside) {
+        prev_id = metro.id;
+        int prev_loc = data_loc, next_loc = -1;
+
+        fread(&next_id, sizeof(next_id), 1, fp);
+
+        printf("%d %d", next_id, next_loc);
+
+        do {
+            fread(&next_loc, sizeof(next_loc), 1, fp);
+            fseek(fp, -1*(int)ind_row_size, SEEK_CUR);
+
+            printf("%d %d",next_id, next_loc);
+
+            fwrite(&prev_id, sizeof(prev_id), 1, fp);
+            fwrite(&prev_loc, sizeof(prev_loc), 1, fp);
+
+            prev_id = next_id;
+            prev_loc = next_loc;
+
+            break;
+        } while (fread(&next_id, sizeof(next_id), 1, fp) != NULL);
+
+        fwrite(&prev_id, sizeof(prev_id), 1, fp);
+        fwrite(&prev_loc, sizeof(prev_loc), 1, fp);
+    }
+    else {
+        fwrite(&metro.id, sizeof(metro.id), 1, fp);
+        fwrite(&data_loc, sizeof(data_loc), 1, fp);
+    }
 
     fclose(fp);
 }
